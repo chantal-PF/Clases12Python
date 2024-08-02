6c84e1ea12b383571ca1a0d253c3fc2892cc3602
Chantal Peñafiel <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-08-01 23:54:38 -0500

    nuevos

---
+++ Arreglos/Unidimensionales-Vectores.py
@@ -0,0 +1,21 @@
+# Declarar una lista unidimensional de fruta
+
+frutas = ["Pera" , "Manzana" , "Piña" , "Fresa"]
+print(frutas)
+frutas.append("Melon")
+print(frutas)
+print(frutas[4])
+
+frutas[4] = "Arandanos"
+print(frutas[4])
+
+# Ejercios de Ordenamiento
+digitos = [5 , 2 , 1 , 0 , 3 , 6 , 7 , 8 , 9]
+print(digitos)
+digitos.sort()
+print(digitos)
+digitosCero = 5*[0]
+print(digitosCero)
+
+# Ejercios se tamañano de la lista
+print("Cantidad de frutas ingrsada:" , len(frutas))
